339ab0bf7c3a937bc5f3e9474cf8b8cbe7285cad
mirikgeroy <tkamir@gmail.com>
2016-05-10 16:56:22 +0300

    Init commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>first php page</title>
+</head>
+
+<body>
+<?php
+
+$name ="Myroslav";
+$age=25;
+
+echo "$name";
+
+echo "<br>car";
+
+echo "my fiend's name is - $name";
+
+?>
+
+</body>
+</html>
