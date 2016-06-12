2eba6af6803ecc9a9ad0ac7728e9b2a91c8bc0c5
Roman Yarmolenko <devxypro@gmail.com>
2016-06-12 10:22:00 +0300

    Add horse

--- chessForm.php
+++ chessForm.php
@@ -1,10 +1,23 @@
 <?php
-include ('chessLib.php');
+include('chessLib.php');
+if (isset($_POST['count'])) {
+    $count = $_POST['count'];
+} else {
+    $count = 0;
+}
+
 ?>
-<form action="chessForm.php" method="POST" name="form">
-  <input type="text" name="count"  value="<?php echo $_POST['count']?>">
-    <input name="submit" type="submit" >
-</form>
+    <form action="chessForm.php" method="POST" name="form">
+        <input type="text" name="count" value="<?php echo $count ?>">
+        <input name="submit" type="submit">
+    </form>
 <?php
-echo renderBoard(boardFill($_POST['count']));
+$x=5;
+$y=5;
+$board = boardFill($count);
+$map=getMap();
+$board[$map[$x]][$y]=3;
+$canRun = canrun($x, $y);
+$boardWithHorse = insertShapes($canRun, $board, 2);
+echo renderBoard($boardWithHorse);
 ?>
--- chessLib.php
+++ chessLib.php
@@ -1,6 +1,6 @@
 <?php
 /**
-tyt tilki funkcii
+ * tyt tilki funkcii
  */
 function boardFill($p)
 {
@@ -22,6 +22,7 @@ function boardFill($p)
         }
         $board[$map[$i]] = $row;
     }
+
     return $board;
 }
 
@@ -32,19 +33,27 @@ function boardFill($p)
 function renderBoard(array $board)
 {
     $result = '<table border="1" align="center" >';
+    $cellSize = 50;
     foreach ($board as $row) {
-        $result.='<tr>'; //
+        $result .= '<tr>'; //
         foreach ($row as $cell) {
-            if ($cell=='0'){
-                $result.='<td height="100" width="100" style="background: green">'.$cell.'</td>';
+            if ($cell == '0') {
+                $result .= renderCell($cellSize, 'green');
             }
-            else{
-                $result.='<td height="100" width="100" style="background: red">'.$cell.'</td>';
+            if ($cell == 'X') {
+                $result .= renderCell($cellSize, 'red');
+            }
+            if ($cell == '2') {
+                $result .= renderCell($cellSize, 'brown');
+            }
+            if ($cell == '3') {
+                $result .= renderCell($cellSize, 'pink');
             }
         }
-        $result.='</tr>';
+        $result .= '</tr>';
     }
-    $result.='</table>';
+    $result .= '</table>';
+
     return $result;
 }
 
@@ -59,5 +68,84 @@ function generator($cellsCount)
             $result[] = $koord;
         }
     }
+
     return $result;
 }
+
+/**
+ * @param integer $cellSize
+ * @param string $color
+ * @return string
+ */
+function renderCell($cellSize, $color)
+{
+    return "<td height='$cellSize' width='$cellSize' style='background: $color'></td>";
+}
+
+/**
+ * @param array $coords
+ * @param array $board
+ * @param integer $element
+ */
+function insertShapes($coords, $board, $element)
+{
+    $map = array(1 => "a", 2 => "b", 3 => "c", 4 => "d", 5 => "e", 6 => "f", 7 => "g", 8 => "h");
+    foreach ($coords as $coord) {
+        $x = $map[$coord[0]];
+        $y = $coord[1];
+        $board[$x][$y] = $element;
+    }
+
+    return $board;
+}
+
+/**
+ * @param integer $x
+ * @param integer $y
+ * @return array
+ */
+function canrun($x, $y)
+{
+    $canrun = [];
+    if ($x >= 1 and $x <= 8 and $y >= 1 and $y <= 8) {
+        for ($i = -2; $i <= 2; $i++) {
+            for ($j = -2; $j <= 2; $j++) {
+                if (abs($i) != abs($j) && $i != 0 && $j != 0) {
+                    $canrun = getCoord($x, $y, $i, $j, $canrun);
+                }
+            }
+        }
+    } else {
+        throw new  Exception('введены неадекватны координати');
+    }
+
+    return $canrun;
+}
+
+/**
+ * @param $x
+ * @param $y
+ * @param $i
+ * @param $j
+ * @param $canrun
+ * @return array
+ */
+function getCoord($x, $y, $i, $j, $canrun)
+{
+    $resX = $x + $i;
+    $resY = $y + $j;
+    if ($resX >= 1 && $resX <= 8 && $resY >= 1 && $resY <= 8) {
+        $canrun[] = [$resX, $resY];
+    }
+
+    return $canrun;
+}
+
+/**
+ * @return array
+ */
+function getMap()
+{
+    return array(1 => "a", 2 => "b", 3 => "c", 4 => "d", 5 => "e", 6 => "f", 7 => "g", 8 => "h");
+}
+
